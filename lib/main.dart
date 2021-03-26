eee411cf0fdd2552c06c46cd7d09fa133478639b
ms471841 <ms471841@.com>
2021-03-26 11:33:13 +0530

    buttons

--- lib/main.dart
+++ lib/main.dart
@@ -4,26 +4,77 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
 
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "images",
+      debugShowCheckedModeBanner: false,
+      title: "network image",
       home: Scaffold(
         appBar: AppBar(
-          centerTitle: true,
-          title: Text("Images"),
+          title: Text("Network image"),
         ),
         body: Center(
-          child: Image.asset(
-            "assets/images/krishna.jpg",
-            fit: BoxFit.fill,
-            height: double.infinity,
-            width: double.infinity,
-          ),
-        ),
+            child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: Text("Click me"),
+                  clipBehavior: Clip.hardEdge,
+                  autofocus: true,
+                ),
+                Text("<--Text Button"),
+              ],
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text("Click me"),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.ac_unit_outlined),
+                  onPressed: () {},
+                  color: Colors.red,
+                  disabledColor: Colors.teal,
+                ),
+                Text("<--Icon Button")
+              ],
+            ),
+            IconButton(
+              icon: Icon(Icons.ac_unit_outlined),
+              onPressed: () {},
+              color: Colors.red,
+              disabledColor: Colors.teal,
+            ),
+            IconButton(
+              icon: Icon(Icons.cake),
+              onPressed: () {},
+              color: Colors.red,
+              disabledColor: Colors.teal,
+            ),
+          ],
+        )),
       ),
     );
   }
